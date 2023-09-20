9b65ac979b586a48ebb2499f9e4b023a054cd03f
Schiughi / Shuji Hosoya <s.hosoya@kakehashi.life>
2023-09-20 16:35:01 +0900

    feat: this is #6

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      Hello World.#5 <Link href="/about">About</Link>
+      Hello World.#6 <Link href="/about">About</Link>
     </div>
   )
 }
